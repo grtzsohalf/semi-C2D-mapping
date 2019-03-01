8ade00b1e607ace7bf835893bc2b038797b278d1
grtzsohalf <piercing.zs0half>
2019-03-01 18:33:59 +0800

    merge

--- path.sh
+++ path.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 ### Please change this PATH according to your environment. ###
-PATH=/home/grtzsohalf/Desktop/semi-C2D-mapping_GAN2:$PATH
-path=/home/grtzsohalf/Desktop/semi-C2D-mapping_GAN2
+PATH=/home/grtzsohalf/Desktop/semi-C2D-mapping:$PATH
+path=/home/grtzsohalf/Desktop/semi-C2D-mapping
 feat_dir=/home/grtzsohalf/Desktop/TIMIT
 exp_dir=/home/grtzsohalf/Desktop/semi-C2D-mapping_TIMIT_matching_exp
